8a4cff769c40178dc6d70ff6fb9428a5b19e94f4
Alina Deeva <d.andreevna.a@gmail.com>
2022-08-25 23:22:09 +0300

    add courier class

---
+++ src/main/java/Courier.java
@@ -0,0 +1,14 @@
+public class Courier {
+
+    private String name;
+    private String password;
+    private String firstName;
+
+
+    public Courier(String name, String password, String firstName) {
+        this.name = name;
+        this.password = password;
+        this.firstName = firstName;
+    }
+
+}
