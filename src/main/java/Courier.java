eafb4faafd25fe23da5b7f87d26af083aea3c585
Alina Deeva <d.andreevna.a@gmail.com>
2022-08-25 23:33:45 +0300

    add courierCredentials

--- src/main/java/Courier.java
+++ src/main/java/Courier.java
@@ -1,14 +1,37 @@
 public class Courier {
 
-    private String name;
+    private String login;
     private String password;
     private String firstName;
 
 
     public Courier(String name, String password, String firstName) {
-        this.name = name;
+        this.login = name;
         this.password = password;
         this.firstName = firstName;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setName(String name) {
+        this.login = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
---
+++ src/main/java/CourierCredentials.java
@@ -0,0 +1,13 @@
+public class CourierCredentials {
+    private String login;
+    private String password;
+
+    public static CourierCredentials from(Courier courier) {
+        return new CourierCredentials(courier.getLogin(), courier.getPassword());
+    }
+
+    public CourierCredentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
